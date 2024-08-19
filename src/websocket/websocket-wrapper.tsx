4a678e0adc9413dc626800db2d5a272a8026304e
Aidin <omurkulovaidin94@gmail.com>
2024-08-20 04:20:45 +0600

    test

--- src/websocket/websocket-wrapper.tsx
+++ src/websocket/websocket-wrapper.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import useWebSocket from '@src/websocket/useWebsocket';
-import { ReactNode, useEffect, useCallback } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { useRouter } from '@/navigation';
 import { IUpdateCookie } from '@my_types/auth-types';
 import { useUser } from '@providers/user-provider';
+import { pushAndRefresh } from '@src/utils';
+import { useLocale } from '@providers/locale-provider';
 
 interface IWebsocketWrapperProps {
     children: ReactNode;
@@ -12,74 +14,63 @@ interface IWebsocketWrapperProps {
 
 const WebsocketWrapper = ({ children }: IWebsocketWrapperProps) => {
     const { updateUser, user } = useUser();
-    const { ws, status } = useWebSocket('ws://localhost:8080');
+    const { locale } = useLocale();
+    const { ws } = useWebSocket('ws://localhost:8080');
+
     const router = useRouter();
 
-    const handleUpdateCookie = useCallback(async (newCookie: IUpdateCookie) => {
+    const handleUpdateCookie = async (newCookie: IUpdateCookie) => {
         await fetch('/api/set-cookie', {
             method: 'POST',
             body: JSON.stringify(newCookie),
             credentials: 'include',
         });
-    }, []);
+    };
 
-    const clearData = useCallback(async () => {
+    const clearData = async () => {
         await fetch('/api/clear-cookie', { method: 'POST' });
         updateUser(null);
-    }, [updateUser]);
+        pushAndRefresh(`/${locale}`);
+    };
 
-    const handleMessage = useCallback(
-        async (event: MessageEvent) => {
-            const { action, data } = JSON.parse(event.data);
+    useEffect(() => {
+        if (ws) {
+            ws.onmessage = async (event) => {
+                const { action, data } = JSON.parse(event.data);
 
-            console.log(data, 'DATA');
+                console.log(data, 'DATA');
 
-            if (action === 'update-token') {
-                await handleUpdateCookie({ user, ...data });
-                updateUser({ ...user!, session_expires: data.session_expires });
-                router.refresh();
-            }
+                if (action === 'update-token') {
+                    await handleUpdateCookie({ user, ...data });
+                    updateUser({ ...user!, session_expires: data.session_expires });
+                    router.refresh();
+                }
 
-            if (action === 'missing-tokens') {
-                console.log(`client missing tokens ${action} ${data}`);
+                if (action === 'missing-tokens') {
+                    console.log(`client missing tokens ${action} ${data}`);
 
+                    await clearData();
+                }
+            };
+
+            ws.onclose = async () => {
+                console.log('Websocket closed');
                 await clearData().then(() => {
                     updateUser(null);
                     router.refresh();
                 });
-            }
-        },
-        [handleUpdateCookie, user, updateUser]
-    );
+            };
 
-    const handleClose = useCallback(async () => {
-        console.log('Websocket closed');
-        await clearData().then(() => {
-            updateUser(null);
-            router.refresh();
-        });
-    }, [clearData, updateUser]);
+            ws.onerror = async () => {
+                await clearData().then(() => {
+                    updateUser(null);
+                    router.refresh();
 
-    const handleError = useCallback(async () => {
-        await clearData().then(() => {
-            updateUser(null);
-            router.refresh();
-        });
-    }, [clearData, updateUser]);
-
-    useEffect(() => {
-        if (ws) {
-            ws.addEventListener('message', handleMessage);
-            ws.addEventListener('close', handleClose);
-            ws.addEventListener('error', handleError);
-
-            return () => {
-                ws.removeEventListener('message', handleMessage);
-                ws.removeEventListener('close', handleClose);
-                ws.removeEventListener('error', handleError);
+                    ws.close();
+                });
             };
         }
-    }, [ws, handleMessage, handleClose, handleError]);
+    }, [ws]);
 
     return <>{children}</>;
 };
