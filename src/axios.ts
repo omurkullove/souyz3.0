963868a09f2b2878f94a91d138febcf05168a5e9
Aidin <omurkulovaidin94@gmail.com>
2024-08-26 01:12:42 +0600

    test

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
-import { API_URL } from './utils/constants';
 
 export const API = axios.create({
-    baseURL: API_URL,
+    baseURL: 'https://souyz3-0.vercel.app/api/v1',
     withCredentials: true,
 });
