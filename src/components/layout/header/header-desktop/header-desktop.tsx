f54d1aadb79223f9744104ddb6254da582126b65
Aidin <omurkulovaidin94@gmail.com>
2024-09-16 09:23:40 +0600

    updated swith-lang

--- src/components/layout/header/header-desktop/header-desktop.tsx
+++ src/components/layout/header/header-desktop/header-desktop.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from '@/navigation';
 import { useLocale } from '@providers/locale-provider';
+import { domain } from '@src/utils/constants';
 import { AnimatePresence, motion, Variants } from 'framer-motion';
 import { FC, useCallback, useState } from 'react';
 import { FaEarthAsia } from 'react-icons/fa6';
@@ -48,7 +49,7 @@ const HeaderDesktop: FC<IProps> = ({ translated }) => {
     const switchLocale = useCallback(
         async (newLocale: Locale) => {
             if (newLocale !== locale) {
-                await fetch('/api/switch-locale', {
+                await fetch(`https://${domain}/api/switch-locale`, {
                     method: 'POST',
                     body: JSON.stringify({ locale: newLocale }),
                     credentials: 'include',
